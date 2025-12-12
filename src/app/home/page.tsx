03adb83bd519ce93d399240a2d978f5260741c36
Luiz <luizfelipemorisco@email.com>
2025-12-12 17:43:25 -0400

    update code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/header";
-import { Hero } from "./components/hero";
+import { SectionSolution } from "./components/section-solutions";
 // import { Footer } from "./components/footer";
 
 export default async function HomePage() {
   return (
     <main className="min-h-screen">
       <Header />
-      <Hero />
+      <SectionSolution />
       {/* <Footer /> */}
     </main>
   );
